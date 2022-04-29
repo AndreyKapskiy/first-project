f2ce6864d0833f74beff4f58105fc0ee4ceee8a5
Andrey Kapskiy <akapsky@yandex.by>
2022-04-29 13:05:14 +0300

    ES6

--- script.js
+++ script.js
@@ -1140,6 +1140,189 @@ function checkSign(num) {
   checkSign(10);
 
 
+  const s = [5, 7, 2];
+  function editInPlace() {
+    // Only change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+    // Using s = [2, 5, 7] would be invalid
+  
+    // Only change code above this line
+  }
+  editInPlace();
+
+
+  function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+
+
+const magic = () => new Date();
+
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+1// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+
+const sum = (...args) => {
+  
+  return args.reduce((a, b) => a + b, 0);
+}
+
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;  
+// Only change code above this line
+
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;  
+// Only change code above this line
+
+
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a]
+
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [, , ...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0; 
+// Only change code above this line
+
+
+const createPerson = (name, age, gender) => ({
+  // Only change code below this line
+  name, age, gender
+ 
+  // Only change code above this line
+});
+
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+
+<html>
+  <body>
+    <!-- Only change code below this line -->
+
+    <!-- Only change code above this line -->
+  </body>
+  <script type="module" src="index.js"></script>
+</html>
+
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+export { uppercaseString, lowercaseString };
+
+
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+
+
+
+
 
 
 
