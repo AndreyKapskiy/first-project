f5b9e6901c1f3e5d18b536f4d5925e32add55028
Andrey Kapskiy <akapsky@yandex.by>
2022-05-06 14:08:02 +0300

    codewars

--- script.js
+++ script.js
@@ -1317,7 +1317,6 @@ import * as stringFunctions from "./string_functions.js";
 // Only change code above this line
 
 stringFunctions.uppercaseString("hello");
-stringFunctions.lowercaseString("WORLD!");
 
 
 let exampleStr = "Let's have fun with regular expressions!";
